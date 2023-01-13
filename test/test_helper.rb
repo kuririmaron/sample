b0b04cce3a3d9d15f050d86e98a35ff5376b5541
kuririmaron <maronpan.maronnashi@gmail.com>
2023-01-13 14:10:36 +0900

    finish

---
+++ test/test_helper.rb
@@ -0,0 +1,11 @@
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+class ActiveSupport::TestCase
+  # parallelize(workers: :number_of_processors)
+  parallelize(workers: 1) # エラーが出るようならこちら
+  fixtures :all
+end
