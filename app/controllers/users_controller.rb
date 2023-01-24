27fea36ab03ffd8c5f80551ec9484b133a513797
kuririmaron <maronpan.maronnashi@gmail.com>
2023-01-24 18:07:09 +0900

    10.2.3フレンドリーフォワーディングまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,35 @@
 class UsersController < ApplicationController
-    def show
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)   # 実装は終わっていないことに注意!
-    if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
     else
       render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -23,5 +38,24 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-  end
+    end
+
+    # beforeアクション
+    
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+
 end
