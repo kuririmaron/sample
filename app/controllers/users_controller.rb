9b9bae07a1211e90ccb484f7e182d21c06ce29bd
kuririmaron <maronpan.maronnashi@gmail.com>
2023-01-25 13:20:53 +0900

    methoderror

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
