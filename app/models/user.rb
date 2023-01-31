5c48f82f05ab50ec29a8ac0fcf7f8bc0c18f6f87
kuririmaron <maronpan.maronnashi@gmail.com>
2023-01-31 17:54:37 +0900

    testh修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,12 +50,12 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
-  # パスワード再設定の属性を設定する
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
-  end
+# パスワード再設定の属性を設定する
+def create_reset_digest
+  self.reset_token = User.new_token
+  update_columns(reset_digest: User.digest(reset_token),
+                reset_sent_at: Time.zone.now)
+end
 
   # パスワード再設定のメールを送信する
   def send_password_reset_email
