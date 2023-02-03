df1c374037f1e0c210b6f1c485ceed03916b50b2
kuririmaron <maronpan.maronnashi@gmail.com>
2023-02-03 10:42:19 +0900

    14.1.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
   has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
